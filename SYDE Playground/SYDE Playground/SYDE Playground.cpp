98d8ecb2aa0fb777f79b20ac80974ad84140256e
Ritalin4Kidz <callum@hands.net.au>
2019-12-29 22:39:40 +1100

    Created AutoClicker

--- SYDE Playground/SYDE Playground/SYDE Playground.cpp
+++ SYDE Playground/SYDE Playground/SYDE Playground.cpp
@@ -30,12 +30,19 @@ CustomAnimationAsset testAnimation;
 vector<string> cheatCodes;
 static ULONG_PTR gdiplusToken;
 static GdiplusStartupInput startupInput;
-
+class AutoClicker : public SYDEWindowGame {
+public:
+	AutoClicker() { m_bg = CustomAsset(40, 20, astVars.get_bmp_as_direct_colour_class_array(L"EngineFiles\\Bitmaps\\bg.bmp", 20, 20)); }
+	virtual ~AutoClicker() {}
+	ConsoleWindow window_draw_game(ConsoleWindow window, int windowWidth, int windowHeight) override;
+private:
+	CustomAsset m_bg;
+	bool _autoClickOn = false;
+};
 int main()
 {
-
-	LPCWSTR title = L"TIC TAC TOE";
-	SYDECredits::_GAMETITLE = "TIC TAC TOE";
+	LPCWSTR title = L"AutoClickerProgram";
+	SYDECredits::_GAMETITLE = "AutoClick";
 	SYDECredits::_ORGANISATION = "Callum Hands \nIn Association With Freebee Games";
 	//CONSOLE SETTINGS
 	//SYDE ENGINE SETTINGS
@@ -54,7 +61,7 @@ int main()
 	SYDEGamePlay::activate_bySplashscreen(astVars.get_electronic_chime_file_path(), start, hOut, window, windowWidth, windowHeight, artVars);
 	//GAMEPLAY
 
-	TicTacToe m_TTT;
+	AutoClicker m_TTT;
 
 	while (true)
 	{
@@ -65,4 +72,27 @@ int main()
 	system("cls");
 	return NULL;
 
+}
+
+ConsoleWindow AutoClicker::window_draw_game(ConsoleWindow window, int windowWidth, int windowHeight)
+{
+	window = m_bg.draw_asset(window, Vector2(0, 0));
+	window.setTextAtPoint(Vector2(0, 19), "AutoClick - Press A To Toggle", BLACK_WHITE_BG);
+	int x,y = 0;
+	if (_autoClickOn)
+	{
+		window = m_bg.draw_asset(window, Vector2(0, 0));
+		POINT cursorPos;
+		GetCursorPos(&cursorPos);
+		//x = 0;
+		x = cursorPos.x;
+		//y = 0;
+		y = cursorPos.y;
+		mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+	}
+	if (SYDEKeyCode::get('A')._CompareState(KEYDOWN))
+	{
+		_autoClickOn = !_autoClickOn;
+	}
+	return window;
 }
